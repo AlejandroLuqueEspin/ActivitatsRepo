abba552dfeb5eb900b08715fe152f02a79085ed0
Alejandro Luque Espin <alejandro.uzumaki@hotmail.com>
2018-10-05 01:58:05 +0200

    ihykgi

--- src/AA1_02/Collisions.h
+++ src/AA1_02/Collisions.h
@@ -3,7 +3,7 @@
 
 inline namespace PlayerCollisions {
 	bool ChecKCollisionHover(Types::vec2 vec2, Types::myRect rect) {
-		if (vec2.x<rect.x&&vec2.y<rect.y&&vec2.x>rect.w&&vec2.y>rect.h)
+		if (vec2.x>rect.x&&vec2.y>rect.y&&vec2.x<rect.w&&vec2.y<rect.h)
 			return true;
 		else
 			return false;
